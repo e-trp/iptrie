9b51c1ccda5c7718e8b4e3a7c397d6e2d6df5a7a
Evgeniy Toropchin <etoropchin@gmail.com>
2026-02-19 11:57:04 +0300

    fix parse string value;  init root from some

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,18 @@ struct IPV4Node {
 }
 
 struct IPV4Trie {
-    root: IPV4Node,
-    size: u64,
+    root: Option<IPV4Node>
 }
 
 impl fmt::Display for IPV4Node {
 
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}.{}.{}.{}/{}",    
-            self.prefix >> 24 & 0xff, self.prefix >> 16 & 0xff, self.prefix >> 8 & 0xff, self.prefix & 0xff, self.lenght
+            self.prefix >> 24,
+            self.prefix >> 16 & 0xff,
+            self.prefix >> 8 & 0xff,
+            self.prefix & 0xff,
+            self.lenght
         )
     }
 }
@@ -34,18 +37,29 @@ impl FromStr for IPV4Node {
      fn from_str(string: &str) -> Result<Self, Self::Err>  {
         let (string_ip, lenght) = string.split_once("/").expect(PARSE_ERROR);
         let prefix = string_ip.split(".")
-                        .enumerate()
-                        .fold(0u32, |s, (i, v)| {
-                            s + v.parse::<u32>().expect(PARSE_ERROR)<< ((3-i) * 8) 
-                            }
-                        );
+            .filter_map(|x| x.parse::<u8>().ok())
+            .enumerate()
+            .map(|(i, val)| (val as u32) << ((3 - i) * 8))
+            .sum();
         Ok(Self{prefix, lenght:lenght.parse::<u8>().expect(PARSE_ERROR)})
      }
 } 
 
 
+impl IPV4Trie {
+
+    pub fn new(root: Option<IPV4Node>) -> Self {
+        Self { root }
+    }
+
+    pub fn insert(&self, node: IPV4Node) {
+
+    }
+}
+
 
 fn main() {
    let root = IPV4Node::from_str("101.102.103.114/32").unwrap();
    println!("integer value {}, string value {}", root.prefix, root);
+   let trie = IPV4Trie::new(Some(root));
 }
