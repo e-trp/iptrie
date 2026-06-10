c0d264842e9f90aa60f18ce8ec4f53bc8196ceee
Evgeniy Toropchin <etoropchin@gmail.com>
2026-06-10 13:27:40 +0300

    mark unused variable

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use std::str::FromStr;
 fn main() {
    let root = Cidr::<u32>::from_str("101.102.103.114/32").unwrap();
    println!("integer value {}, string value {}", root.net, root);
-   let trie = CidrTrie::<Cidr<u32>>::new(Some(root));
+   let _trie = CidrTrie::<Cidr<u32>>::new(Some(root));
 }
