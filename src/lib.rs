9d6680b335ad871354ed43d603097f486c402522
Evgeniy Toropchin <etoropchin@gmail.com>
2026-06-29 16:20:14 +0300

    python bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+use pyo3::prelude::*;
+
 pub mod ip;
+pub mod py;
+
+#[pymodule]
+fn iptrie(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<py::PyTrie>()?;
+    Ok(())
+}
---
+++ src/py.rs
@@ -0,0 +1,31 @@
+use crate::ip::{Cidr, CidrTrie};
+use pyo3::prelude::*;
+
+#[pyclass]
+pub struct PyTrie {
+    inner: CidrTrie<Cidr<u32>>,
+}
+
+#[pymethods]
+impl PyTrie {
+    #[new]
+    fn new() -> Self {
+        Self {
+            inner: CidrTrie::new(),
+        }
+    }
+
+    fn insert(&mut self, cidr: &str) -> PyResult<()> {
+        let parsed: Cidr<u32> = cidr
+            .parse()
+            .map_err(|e| pyo3::exceptions::PyValueError::new_err(e))?;
+
+        self.inner.insert(parsed);
+        Ok(())
+    }
+
+    fn search(&self, cidr: &str) -> Option<String> {
+        let parsed: Cidr<u32> = cidr.parse().ok()?;
+        self.inner.search(parsed).map(|v| v.to_string())
+    }
+}
