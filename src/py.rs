a14781819f56823ec21ccbe4fd6d24bb98d92417
Evgeniy Toropchin <etoropchin@gmail.com>
2026-07-10 13:42:39 +0300

    impl python method

--- src/py.rs
+++ src/py.rs
@@ -15,17 +15,23 @@ impl PyTrie {
         }
     }
 
-    fn insert(&mut self, cidr: &str) -> PyResult<()> {
-        let parsed: Cidr<u32> = cidr
-            .parse()
-            .map_err(pyo3::exceptions::PyValueError::new_err)?;
-
-        self.inner.insert(parsed);
-        Ok(())
+    fn insert(&mut self, cidr: &str) -> Option<bool> {
+        let parsed: Cidr<u32> = cidr.parse().ok()?;
+        self.inner.insert(parsed)
     }
 
     fn search(&self, cidr: &str) -> Option<String> {
         let parsed: Cidr<u32> = cidr.parse().ok()?;
         self.inner.search(&parsed).map(|v| v.to_string())
+    }
+
+    fn search_supernets(&self, cidr: &str) -> Option<Vec<String>> {
+        let parsed: Cidr<u32> = cidr.parse().ok()?;
+        self.inner.search_supernets(&parsed).map(|result| {
+            result
+                .into_iter()
+                .map(|i| i.to_string())
+                .collect::<Vec<String>>()
+        })
     }
 }
